fa2b64776195eea60f7bea41869f087dc2761953
Payton Swick <payton@foolord.com>
2022-12-16 16:46:55 -0500

    Add updateSomeFields wpcom-store action
    This allows updating the whole state directly from a copy of it rather
    than setting an individual field.

--- client/my-sites/checkout/composite-checkout/hooks/wpcom-store.ts
+++ client/my-sites/checkout/composite-checkout/hooks/wpcom-store.ts
@@ -19,6 +19,7 @@ type WpcomStoreAction =
 			type: 'APPLY_DOMAIN_CONTACT_VALIDATION_RESULTS';
 			payload: ManagedContactDetailsErrors;
 	  }
+	| { type: 'UPDATE_SOME_FIELDS'; payload: ManagedContactDetails }
 	| { type: 'UPDATE_DOMAIN_CONTACT_FIELDS'; payload: DomainContactDetails }
 	| { type: 'SET_RECAPTCHA_CLIENT_ID'; payload: number }
 	| { type: 'UPDATE_VAT_ID'; payload: string }
@@ -52,6 +53,8 @@ export function useWpcomStore(): void {
 			case 'UPDATE_DOMAIN_CONTACT_FIELDS': {
 				return updaters.updateDomainContactFields( state, action.payload );
 			}
+			case 'UPDATE_SOME_FIELDS':
+				return updaters.updateSomeFields( state, action.payload );
 			case 'UPDATE_VAT_ID':
 				return updaters.updateVatId( state, action.payload );
 			case 'UPDATE_PHONE':
@@ -121,6 +124,10 @@ export function useWpcomStore(): void {
 
 			setRecaptchaClientId( payload: number ): WpcomStoreAction {
 				return { type: 'SET_RECAPTCHA_CLIENT_ID', payload };
+			},
+
+			updateSomeFields( payload: ManagedContactDetails ): WpcomStoreAction {
+				return { type: 'UPDATE_SOME_FIELDS', payload };
 			},
 
 			updateDomainContactFields( payload: DomainContactDetails ): WpcomStoreAction {
