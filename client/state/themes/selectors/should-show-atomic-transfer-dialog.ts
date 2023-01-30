6aafb766269c64e0d2c65b2349dacc20f56db205
Valter Lorran <valterlorran@hotmail.com>
2023-01-30 18:24:32 -0300

    Adding comment to new selector

--- client/state/themes/selectors/should-show-atomic-transfer-dialog.ts
+++ client/state/themes/selectors/should-show-atomic-transfer-dialog.ts
@@ -2,6 +2,11 @@ import 'calypso/state/themes/init';
 import { AppState } from 'calypso/types';
 
 /**
+ * Checks if the atomic transfer dialog should be shown for a given theme.
+ *
+ * @param state The app global state
+ * @param themeId The theme ID
+ * @returns
  */
 export function shouldShowAtomicTransferDialog( state: AppState, themeId: string ) {
 	return (
