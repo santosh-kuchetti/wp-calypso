e08af5cdafcb982acb47a3cb92550ddbc2706ee8
Valter Lorran <valterlorran@hotmail.com>
2023-01-31 09:31:19 -0300

    Adding comment for hasAtomicTransferDialogAccepted

--- client/state/themes/selectors/has-atomic-transfer-dialog-accepted.js
+++ client/state/themes/selectors/has-atomic-transfer-dialog-accepted.js
@@ -1,6 +1,11 @@
 import 'calypso/state/themes/init';
 
 /**
+ * Checks if the atomic transfer dialog has been accepted.
+ *
+ * @param {Object} state   Global state tree
+ * @param {string} themeId Theme ID to check if the atomic transfer dialog has been accepted.
+ * @returns True if the atomic transfer dialog has been accepted. Otherwise, False.
  */
 export function hasAtomicTransferDialogAccepted( state, themeId ) {
 	return (
