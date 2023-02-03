d7ed10c44b8a2010e469c5644a347f0c8b26bb3d
Don Lair <don.lair@gmail.com>
2023-02-03 13:21:05 -0500

    Creating standalone version of the plans step for the paid media signup flow

---
+++ client/signup/steps/plans-pm/index.jsx
@@ -0,0 +1,358 @@
+import {
+	planHasFeature,
+	FEATURE_UPLOAD_THEMES_PLUGINS,
+	getPlan,
+	PLAN_FREE,
+	isEcommerce,
+} from '@automattic/calypso-products';
+import { getUrlParts } from '@automattic/calypso-url';
+import { Button } from '@automattic/components';
+import { isDesktop, subscribeIsDesktop } from '@automattic/viewport';
+import classNames from 'classnames';
+import i18n, { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+import { parse as parseQs } from 'qs';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import QueryPlans from 'calypso/components/data/query-plans';
+import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
+import MarketingMessage from 'calypso/components/marketing-message';
+import Notice from 'calypso/components/notice';
+import { getTld, isSubdomain } from 'calypso/lib/domains';
+import { getSiteTypePropertyValue } from 'calypso/lib/signup/site-type';
+import StepWrapper from 'calypso/signup/step-wrapper';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { errorNotice } from 'calypso/state/notices/actions';
+import { getPlanSlug } from 'calypso/state/plans/selectors';
+import hasInitializedSites from 'calypso/state/selectors/has-initialized-sites';
+import { saveSignupStep, submitSignupStep } from 'calypso/state/signup/progress/actions';
+import { getSiteType } from 'calypso/state/signup/steps/site-type/selectors';
+import { getSiteBySlug } from 'calypso/state/sites/selectors';
+import PlansFeaturesMainPM from './plans-features-main-pm';
+import '../plans/style.scss';
+import './style.scss';
+
+export class PlansStepPM extends Component {
+	state = {
+		isDesktop: isDesktop(),
+	};
+
+	componentDidMount() {
+		this.unsubscribe = subscribeIsDesktop( ( matchesDesktop ) =>
+			this.setState( { isDesktop: matchesDesktop } )
+		);
+		this.props.saveSignupStep( { stepName: this.props.stepName } );
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	onSelectPlan = ( cartItem ) => {
+		const {
+			additionalStepData,
+			stepSectionName,
+			stepName,
+			flowName,
+			themeSlugWithRepo,
+			launchSite,
+			goToNextStep,
+		} = this.props;
+
+		if ( cartItem ) {
+			this.props.recordTracksEvent( 'calypso_signup_plan_select', {
+				product_slug: cartItem.product_slug,
+				free_trial: cartItem.free_trial,
+				from_section: stepSectionName ? stepSectionName : 'default',
+			} );
+
+			// If we're inside the store signup flow and the cart item is a Business or eCommerce Plan,
+			// set a flag on it. It will trigger Automated Transfer when the product is being
+			// activated at the end of the checkout process.
+			if (
+				flowName === 'ecommerce' &&
+				planHasFeature( cartItem.product_slug, FEATURE_UPLOAD_THEMES_PLUGINS )
+			) {
+				cartItem.extra = Object.assign( cartItem.extra || {}, {
+					is_store_signup: true,
+				} );
+			}
+		} else {
+			this.props.recordTracksEvent( 'calypso_signup_free_plan_select', {
+				from_section: stepSectionName ? stepSectionName : 'default',
+			} );
+		}
+
+		const step = {
+			stepName,
+			stepSectionName,
+			cartItem,
+			...additionalStepData,
+		};
+
+		const signupVals = {
+			cartItem,
+			...( themeSlugWithRepo && { themeSlugWithRepo } ),
+			...( launchSite && { comingSoon: 0 } ),
+		};
+
+		if ( cartItem && isEcommerce( cartItem ) ) {
+			signupVals.themeSlugWithRepo = 'pub/twentytwentytwo';
+		}
+
+		this.props.submitSignupStep( step, signupVals );
+		goToNextStep();
+	};
+
+	getDomainName() {
+		return (
+			this.props.signupDependencies.domainItem && this.props.signupDependencies.domainItem.meta
+		);
+	}
+
+	getCustomerType() {
+		if ( this.props.customerType ) {
+			return this.props.customerType;
+		}
+
+		const customerType =
+			getSiteTypePropertyValue( 'slug', this.props.siteType, 'customerType' ) || 'personal';
+
+		return customerType;
+	}
+
+	handleFreePlanButtonClick = () => {
+		this.onSelectPlan( null ); // onUpgradeClick expects a cart item -- null means Free Plan.
+	};
+
+	getIntervalType() {
+		const urlParts = getUrlParts( typeof window !== 'undefined' ? window.location?.href : '' );
+		const intervalType = urlParts?.searchParams.get( 'intervalType' );
+
+		if ( [ 'yearly', 'monthly' ].includes( intervalType ) ) {
+			return intervalType;
+		}
+
+		// Default value
+		return 'yearly';
+	}
+
+	plansFeaturesList() {
+		const { isLaunchPage, selectedSite, planTypes, flowName, isReskinned } = this.props;
+
+		let errorDisplay;
+		if ( 'invalid' === this.props.step?.status ) {
+			errorDisplay = (
+				<div>
+					<Notice status="is-error" showDismiss={ false }>
+						{ this.props.step.errors.message }
+					</Notice>
+				</div>
+			);
+		}
+
+		return (
+			<div>
+				{ errorDisplay }
+				<PlansFeaturesMainPM
+					site={ selectedSite || {} } // `PlanFeaturesMain` expects a default prop of `{}` if no site is provided
+					showFAQ={ true }
+					hideFreePlan={ true }
+					isInSignup={ true }
+					isLaunchPage={ isLaunchPage }
+					intervalType={ this.getIntervalType() }
+					onUpgradeClick={ this.onSelectPlan }
+					domainName={ this.getDomainName() }
+					customerType={ this.getCustomerType() }
+					plansWithScroll={ this.state.isDesktop }
+					planTypes={ planTypes }
+					flowName={ flowName }
+					isAllPaidPlansShown={ true }
+					shouldShowPlansFeatureComparison={ this.state.isDesktop } // Show feature comparison layout in signup flow and desktop resolutions
+					isReskinned={ isReskinned }
+				/>
+			</div>
+		);
+	}
+
+	renderLoading() {
+		return (
+			<div className="plans__loading">
+				<LoadingEllipsis active />
+			</div>
+		);
+	}
+
+	getHeaderText() {
+		const { headerText, translate, eligibleForProPlan, locale, isOnboarding2023PricingGrid } =
+			this.props;
+
+		if ( headerText ) {
+			return headerText;
+		}
+
+		if ( eligibleForProPlan ) {
+			return 'en' === locale || i18n.hasTranslation( 'Choose the right plan for you' )
+				? translate( 'Choose the right plan for you' )
+				: translate( 'Choose the plan that’s right for you' );
+		}
+
+		if ( isOnboarding2023PricingGrid ) {
+			return translate( 'Choose your flavor of WordPress' );
+		}
+
+		if ( this.state.isDesktop ) {
+			return translate( 'Choose a plan' );
+		}
+
+		return translate( "Pick a plan that's right for you." );
+	}
+
+	getSubHeaderText() {
+		const { translate } = this.props;
+
+		const freePlanButton = <Button onClick={ this.handleFreePlanButtonClick } borderless />;
+
+		if ( this.state.isDesktop ) {
+			return translate(
+				"Pick one that's right for you and unlock features that help you grow. Or {{link}}start with a free site{{/link}}.",
+				{ components: { link: freePlanButton } }
+			);
+		}
+
+		return translate( 'Choose a plan or {{link}}start with a free site{{/link}}.', {
+			components: { link: freePlanButton },
+		} );
+	}
+
+	plansFeaturesSelection() {
+		const {
+			flowName,
+			stepName,
+			positionInFlow,
+			translate,
+			hasInitializedSitesBackUrl,
+			steps,
+			isOnboarding2023PricingGrid,
+		} = this.props;
+
+		const headerText = this.getHeaderText();
+		const fallbackHeaderText = this.props.fallbackHeaderText || headerText;
+		const subHeaderText = this.getSubHeaderText();
+		const fallbackSubHeaderText = this.props.fallbackSubHeaderText || subHeaderText;
+
+		let backUrl;
+		let backLabelText;
+
+		if ( 0 === positionInFlow && hasInitializedSitesBackUrl ) {
+			backUrl = hasInitializedSitesBackUrl;
+			backLabelText = translate( 'Back to Sites' );
+		}
+
+		let queryParams;
+		if ( ! isNaN( Number( positionInFlow ) ) && 0 !== positionInFlow ) {
+			const previousStepName = steps[ this.props.positionInFlow - 1 ];
+			const previousStep = this.props.progress?.[ previousStepName ];
+
+			const isComingFromUseYourDomainStep = 'use-your-domain' === previousStep?.stepSectionName;
+
+			if ( isComingFromUseYourDomainStep ) {
+				queryParams = {
+					...this.props.queryParams,
+					step: 'transfer-or-connect',
+					initialQuery: previousStep?.siteUrl,
+				};
+			}
+		}
+
+		return (
+			<>
+				<StepWrapper
+					flowName={ flowName }
+					stepName={ stepName }
+					positionInFlow={ positionInFlow }
+					headerText={ headerText }
+					fallbackHeaderText={ fallbackHeaderText }
+					subHeaderText={ subHeaderText }
+					fallbackSubHeaderText={ fallbackSubHeaderText }
+					isWideLayout={ ! isOnboarding2023PricingGrid }
+					isExtraWideLayout={ isOnboarding2023PricingGrid }
+					stepContent={ this.plansFeaturesList() }
+					allowBackFirstStep={ !! hasInitializedSitesBackUrl }
+					backUrl={ backUrl }
+					backLabelText={ backLabelText }
+					queryParams={ queryParams }
+				/>
+			</>
+		);
+	}
+
+	render() {
+		const classes = classNames( 'plans plans-step', {
+			'in-vertically-scrolled-plans-experiment':
+				! this.props.isOnboarding2023PricingGrid && this.props.isInVerticalScrollingPlansExperiment,
+			'has-no-sidebar': false,
+			'is-wide-layout': true,
+		} );
+
+		return (
+			<>
+				<QueryPlans />
+				<MarketingMessage path="signup/plans" />
+				<div className={ classes }>{ this.plansFeaturesSelection() }</div>
+			</>
+		);
+	}
+}
+
+PlansStepPM.propTypes = {
+	additionalStepData: PropTypes.object,
+	disableBloggerPlanWithNonBlogDomain: PropTypes.bool,
+	goToNextStep: PropTypes.func.isRequired,
+	hideFreePlan: PropTypes.bool,
+	selectedSite: PropTypes.object,
+	stepName: PropTypes.string.isRequired,
+	stepSectionName: PropTypes.string,
+	customerType: PropTypes.string,
+	translate: PropTypes.func.isRequired,
+	planTypes: PropTypes.array,
+	flowName: PropTypes.string,
+};
+
+/**
+ * Checks if the domainItem picked in the domain step is a top level .blog domain -
+ * we only want to make Blogger plan available if it is.
+ *
+ * @param {Object} domainItem domainItem object stored in the "choose domain" step
+ * @returns {boolean} is .blog domain registration
+ */
+export const isDotBlogDomainRegistration = ( domainItem ) => {
+	if ( ! domainItem ) {
+		return false;
+	}
+	const { is_domain_registration, meta } = domainItem;
+
+	return is_domain_registration && getTld( meta ) === 'blog';
+};
+
+export default connect(
+	( state, { path, signupDependencies: { siteSlug, domainItem } } ) => ( {
+		// Blogger plan is only available if user chose either a free domain or a .blog domain registration
+		disableBloggerPlanWithNonBlogDomain:
+			domainItem && ! isSubdomain( domainItem.meta ) && ! isDotBlogDomainRegistration( domainItem ),
+		// This step could be used to set up an existing site, in which case
+		// some descendants of this component may display discounted prices if
+		// they apply to the given site.
+		selectedSite: siteSlug ? getSiteBySlug( state, siteSlug ) : null,
+		customerType: parseQs( path.split( '?' ).pop() ).customerType,
+		siteType: getSiteType( state ),
+		hasInitializedSitesBackUrl: hasInitializedSites( state ) ? '/sites/' : false,
+		isLoadingExperiment: false,
+		// IMPORTANT NOTE: The following is always set to true. It's a hack to resolve the bug reported
+		// in https://github.com/Automattic/wp-calypso/issues/50896, till a proper cleanup and deploy of
+		// treatment for the `vertical_plan_listing_v2` experiment is implemented.
+		isInVerticalScrollingPlansExperiment: true,
+		plansLoaded: Boolean( getPlanSlug( state, getPlan( PLAN_FREE )?.getProductId() || 0 ) ),
+	} ),
+	{ recordTracksEvent, saveSignupStep, submitSignupStep, errorNotice }
+)( localize( PlansStepPM ) );
---
+++ client/signup/steps/plans-pm/plans-features-main-pm.jsx
@@ -0,0 +1,237 @@
+import {
+	chooseDefaultCustomerType,
+	findPlansKeys,
+	getPlan,
+	getPopularPlanSpec,
+	isFreePlan,
+	TYPE_FREE,
+	TYPE_PERSONAL,
+	TYPE_PREMIUM,
+	TYPE_BUSINESS,
+	TYPE_ECOMMERCE,
+	TERM_MONTHLY,
+	TERM_ANNUALLY,
+	TERM_BIENNIALLY,
+	TERM_TRIENNIALLY,
+	PLAN_PERSONAL,
+	GROUP_WPCOM,
+} from '@automattic/calypso-products';
+import classNames from 'classnames';
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import QueryPlans from 'calypso/components/data/query-plans';
+import QuerySitePlans from 'calypso/components/data/query-site-plans';
+import QuerySites from 'calypso/components/data/query-sites';
+import PlanFeaturesComparison from 'calypso/my-sites/plan-features-comparison';
+import PlanTypeSelector from 'calypso/my-sites/plans-features-main/plan-type-selector';
+import PlanFAQ from 'calypso/my-sites/plans-features-main/plansStepFaq';
+import canUpgradeToPlan from 'calypso/state/selectors/can-upgrade-to-plan';
+import isEligibleForWpComMonthlyPlan from 'calypso/state/selectors/is-eligible-for-wpcom-monthly-plan';
+import {
+	getSitePlan,
+	getSiteSlug,
+	isJetpackSite,
+	isJetpackSiteMultiSite,
+} from 'calypso/state/sites/selectors';
+
+import 'calypso/my-sites/plans-features-main/style.scss';
+
+export class PlansFeaturesMainPM extends Component {
+	showPricingGrid() {
+		const {
+			basePlansPath,
+			customerType,
+			domainName,
+			isInSignup,
+			isJetpack,
+			isLandingPage,
+			isLaunchPage,
+			flowName,
+			onUpgradeClick,
+			selectedFeature,
+			selectedPlan,
+			withDiscount,
+			discountEndDate,
+			redirectTo,
+			siteId,
+			plansWithScroll,
+		} = this.props;
+
+		const plans = this.getPlans();
+
+		return (
+			<div
+				className={ classNames(
+					'plans-features-main__group',
+					'is-wpcom',
+					`is-customer-${ customerType }`,
+					{
+						'is-scrollable': plansWithScroll,
+					}
+				) }
+				data-e2e-plans="wpcom"
+			>
+				<PlanFeaturesComparison
+					basePlansPath={ basePlansPath }
+					domainName={ domainName }
+					isInSignup={ isInSignup }
+					isLandingPage={ isLandingPage }
+					isLaunchPage={ isLaunchPage }
+					onUpgradeClick={ onUpgradeClick }
+					plans={ plans }
+					flowName={ flowName }
+					redirectTo={ redirectTo }
+					visiblePlans={ plans }
+					selectedFeature={ selectedFeature }
+					selectedPlan={ selectedPlan }
+					withDiscount={ withDiscount }
+					discountEndDate={ discountEndDate }
+					withScroll={ plansWithScroll }
+					popularPlanSpec={ getPopularPlanSpec( {
+						flowName,
+						customerType,
+						isJetpack,
+						availablePlans: plans,
+					} ) }
+					siteId={ siteId }
+					isReskinned={ true }
+				/>
+			</div>
+		);
+	}
+
+	getPlanBillingPeriod( intervalType, defaultValue = null ) {
+		const plans = {
+			monthly: TERM_MONTHLY,
+			yearly: TERM_ANNUALLY,
+			'2yearly': TERM_BIENNIALLY,
+			'3yearly': TERM_TRIENNIALLY,
+		};
+
+		return plans[ intervalType ] || defaultValue || TERM_ANNUALLY;
+	}
+
+	getPlans() {
+		const { intervalType, selectedPlan, hideFreePlan } = this.props;
+
+		const term = this.getPlanBillingPeriod( intervalType, getPlan( selectedPlan )?.term );
+		let plans = [
+			findPlansKeys( { group: GROUP_WPCOM, type: TYPE_FREE } )[ 0 ],
+			findPlansKeys( { group: GROUP_WPCOM, term, type: TYPE_PERSONAL } )[ 0 ],
+			findPlansKeys( { group: GROUP_WPCOM, term, type: TYPE_PREMIUM } )[ 0 ],
+			findPlansKeys( { group: GROUP_WPCOM, term, type: TYPE_BUSINESS } )[ 0 ],
+			findPlansKeys( { group: GROUP_WPCOM, term, type: TYPE_ECOMMERCE } )[ 0 ],
+		].filter( ( el ) => el );
+
+		if ( hideFreePlan ) {
+			plans = plans.filter( ( planSlug ) => ! isFreePlan( planSlug ) );
+		}
+		return plans;
+	}
+
+	render() {
+		const { siteId, planTypeSelectorProps } = this.props;
+		const plans = this.getPlans();
+
+		return (
+			<div className={ classNames( 'plans-features-main' ) }>
+				<QueryPlans />
+				<QuerySites siteId={ siteId } />
+				<QuerySitePlans siteId={ siteId } />
+				<div className="plans-features-main__notice" />
+				<PlanTypeSelector { ...planTypeSelectorProps } kind="interval" plans={ plans } />
+				{ this.showPricingGrid() }
+				<PlanFAQ />
+			</div>
+		);
+	}
+}
+
+PlansFeaturesMainPM.propTypes = {
+	redirectToAddDomainFlow: PropTypes.bool,
+	domainAndPlanPackage: PropTypes.string,
+	basePlansPath: PropTypes.string,
+	hideFreePlan: PropTypes.bool,
+	hidePersonalPlan: PropTypes.bool,
+	hidePremiumPlan: PropTypes.bool,
+	customerType: PropTypes.string,
+	flowName: PropTypes.string,
+	intervalType: PropTypes.oneOf( [ 'monthly', 'yearly' ] ),
+	isChatAvailable: PropTypes.bool,
+	isInSignup: PropTypes.bool,
+	isLandingPage: PropTypes.bool,
+
+	onUpgradeClick: PropTypes.func,
+	redirectTo: PropTypes.string,
+	selectedFeature: PropTypes.string,
+	selectedPlan: PropTypes.string,
+	showFAQ: PropTypes.bool,
+	siteId: PropTypes.number,
+	siteSlug: PropTypes.string,
+	isAllPaidPlansShown: PropTypes.bool,
+	plansWithScroll: PropTypes.bool,
+	planTypes: PropTypes.array,
+	isReskinned: PropTypes.bool,
+	isPlansInsideStepper: PropTypes.bool,
+	planTypeSelector: PropTypes.string,
+};
+
+PlansFeaturesMainPM.defaultProps = {
+	basePlansPath: null,
+	hideFreePlan: false,
+	hidePersonalPlan: false,
+	hidePremiumPlan: false,
+	intervalType: 'yearly',
+	isChatAvailable: false,
+	showFAQ: true,
+	siteId: null,
+	siteSlug: '',
+	plansWithScroll: false,
+	isReskinned: false,
+	planTypeSelector: 'interval',
+	isPlansInsideStepper: false,
+};
+
+export default connect( ( state, props ) => {
+	const siteId = props.site?.ID;
+	const sitePlan = getSitePlan( state, siteId );
+	const sitePlanSlug = sitePlan?.product_slug;
+	const siteSlug = getSiteSlug( state, siteId );
+	const eligibleForWpcomMonthlyPlans = isEligibleForWpComMonthlyPlan( state, siteId );
+
+	let customerType = chooseDefaultCustomerType( {
+		currentCustomerType: props.customerType,
+		selectedPlan: props.selectedPlan,
+		sitePlan,
+	} );
+	// Make sure the plans for the default customer type can be purchased.
+	if (
+		! props.customerType &&
+		customerType === 'personal' &&
+		! canUpgradeToPlan( state, siteId, PLAN_PERSONAL )
+	) {
+		customerType = 'business';
+	}
+
+	const planTypeSelectorProps = {
+		basePlansPath: props.basePlansPath,
+		isInSignup: props.isInSignup,
+		eligibleForWpcomMonthlyPlans: eligibleForWpcomMonthlyPlans,
+		isPlansInsideStepper: props.isPlansInsideStepper,
+		intervalType: props.intervalType,
+		customerType: customerType,
+		hidePersonalPlan: props.hidePersonalPlan,
+		siteSlug,
+	};
+
+	return {
+		isJetpack: isJetpackSite( state, siteId ),
+		isMultisite: isJetpackSiteMultiSite( state, siteId ),
+		siteId,
+		siteSlug,
+		sitePlanSlug,
+		planTypeSelectorProps,
+	};
+} )( localize( PlansFeaturesMainPM ) );
