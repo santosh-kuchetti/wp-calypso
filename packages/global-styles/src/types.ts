f83464c7d540977efd16b2510f50e4bdbc91e286
arthur <arthur.chu@automattic.com>
2023-02-07 10:24:09 +0800

    Global Styles: Implement color palette variations

---
+++ packages/global-styles/src/types.ts
@@ -0,0 +1,28 @@
+export interface Color {
+	color: string;
+	name: string;
+	slug: string;
+}
+
+export interface GlobalStylesObject {
+	id?: number;
+	settings: {
+		color?: {
+			palette: {
+				theme: Color[];
+			};
+		};
+	};
+	styles: {
+		elements?: {
+			heading: {
+				typography: {
+					fontFamily: string;
+				};
+			};
+		};
+		typography?: {
+			fontFamily: string;
+		};
+	};
+}
